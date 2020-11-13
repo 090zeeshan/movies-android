ada3671e6783ffdf77eb3ec161ca1230e41919f5
zeeshan <zeeshan.afzal@venturedive.com>
2020-11-13 15:54:17 +0500

    android navigation setup

--- app/src/main/java/com/vd/movies/MainActivity.kt
+++ app/src/main/java/com/vd/movies/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.vd.movies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerToggle: ActionBarDrawerToggle
+    lateinit var navController: NavController;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = navHostFragment.findNavController()
+        setupNavDrawer()
+    }
+
+    private fun setupNavDrawer() {
+        supportActionBar?.let {
+            it.setHomeButtonEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+        }
+        drawerToggle =
+            ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        drawerLayout.navView.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return false
     }
 }
--- app/src/main/java/com/vd/movies/ui/listing/ListingFragment.kt
+++ app/src/main/java/com/vd/movies/ui/listing/ListingFragment.kt
@@ -1,11 +1,13 @@
 package com.vd.movies.ui.listing
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.vd.movies.R
+import kotlinx.android.synthetic.main.fragment_listing.*
 
 class ListingFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +19,16 @@ class ListingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_listing, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let{
+            tv.text = ListingFragmentArgs.fromBundle(it).listingType.toString()
+        }
+
+
     }
 }
